e326fa0b31e1bbcef895b19d45d4d53d51c85071
mconstant <bobbaxter@protonmail.com>
2024-08-06 19:40:29 -0400

    extra stuff for bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+cat <<EOF > app/.env
+# this is secret stuff. When you change it don't commit it to the remote
+# an initial version of this file is provided on purpose to establish a VERSION
+VERSION=0.0.0
+TOP_SECRET_MESSAGE="This is another secret message"
+EOF
+
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
